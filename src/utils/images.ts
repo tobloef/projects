f211b6fbe362bb2a166d6413e5890da50c22d45a
Tobias Løfgren <tobloef@gmail.com>
2022-08-16 01:30:31 +0200

    Fix images

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,66 +1,68 @@
-export const markantThumbnail = "./images/markant/thumbnail.png";
-export const markantExample = "./images/markant/example.png";
+const prefix = "/projects";
 
-export const setSolverThumbnail = "./images/set-solver/thumbnail.png";
-export const setSolverExample = "./images/set-solver/example.jpeg";
+export const markantThumbnail = `${prefix}/images/markant/thumbnail.png`;
+export const markantExample = `${prefix}/images/markant/example.png`;
 
-export const putPixThumbnail = "./images/3d-renderer/thumbnail.png";
+export const setSolverThumbnail = `${prefix}/images/set-solver/thumbnail.png`;
+export const setSolverExample = `${prefix}/images/set-solver/example.jpeg`;
 
-export const birdInParadiseThumbnail = "./images/bird-in-paradise/thumbnail.jpeg"
+export const putPixThumbnail = `${prefix}/images/3d-renderer/thumbnail.png`;
 
-export const ctfThumbnail = "./images/ctf/thumbnail.png"
-export const ctfExample = "./images/ctf/example.png"
+export const birdInParadiseThumbnail = `${prefix}/images/bird-in-paradise/thumbnail.jpeg`
 
-export const cardFuThumbnail = "./images/card-fu/thumbnail.png"
+export const ctfThumbnail = `${prefix}/images/ctf/thumbnail.png`
+export const ctfExample = `${prefix}/images/ctf/example.png`
 
-export const co2Thumbnail = "./images/co2/thumbnail.png"
+export const cardFuThumbnail = `${prefix}/images/card-fu/thumbnail.png`
 
-export const yotoThumbnail = "./images/yoto/thumbnail.png"
+export const co2Thumbnail = `${prefix}/images/co2/thumbnail.png`
 
-export const roll20DiceScreenshot = "./images/roll20-dice/screenshot.png"
+export const yotoThumbnail = `${prefix}/images/yoto/thumbnail.png`
 
-export const text2mindmapScreenshot = "./images/text2mindmap/example.png"
+export const roll20DiceScreenshot = `${prefix}/images/roll20-dice/screenshot.png`
 
-export const faceGeneratorExample = "./images/face-generator/example.png"
+export const text2mindmapScreenshot = `${prefix}/images/text2mindmap/example.png`
 
-export const nftFrontpage = "./images/nft/frontpage.png"
-export const nftNft = "./images/nft/nft.png"
-export const nftNfts = "./images/nft/nfts.png"
-export const nftUser = "./images/nft/user.png"
-export const nftTrades = "./images/nft/trades.png"
+export const faceGeneratorExample = `${prefix}/images/face-generator/example.png`
 
-export const creativeExample = "./images/creative/example.png"
-export const creativeMidiController = "./images/creative/midi-controller.png"
+export const nftFrontpage = `${prefix}/images/nft/frontpage.png`
+export const nftNft = `${prefix}/images/nft/nft.png`
+export const nftNfts = `${prefix}/images/nft/nfts.png`
+export const nftUser = `${prefix}/images/nft/user.png`
+export const nftTrades = `${prefix}/images/nft/trades.png`
 
-export const neuralnetThumbnail = "./images/neuralnet/thumbnail.jpeg"
+export const creativeExample = `${prefix}/images/creative/example.png`
+export const creativeMidiController = `${prefix}/images/creative/midi-controller.png`
 
-export const base64Example = "./images/base64/example.png"
+export const neuralnetThumbnail = `${prefix}/images/neuralnet/thumbnail.jpeg`
 
-export const mapExample = "./images/map/example.png"
+export const base64Example = `${prefix}/images/base64/example.png`
 
-export const nemCvThumbnail = "./images/nem-cv/thumbnail.png"
-export const nemCvExample = "./images/nem-cv/example.png"
+export const mapExample = `${prefix}/images/map/example.png`
 
-export const fooblogExample = "./images/fooblog/example.png"
+export const nemCvThumbnail = `${prefix}/images/nem-cv/thumbnail.png`
+export const nemCvExample = `${prefix}/images/nem-cv/example.png`
 
-export const realtimeEditorExample = "./images/realtime-editor/example.png"
+export const fooblogExample = `${prefix}/images/fooblog/example.png`
 
-export const vowkeeperExample = "./images/vowkeeper/example.png"
+export const realtimeEditorExample = `${prefix}/images/realtime-editor/example.png`
 
-export const polychatExample = "./images/polychat/example.png"
+export const vowkeeperExample = `${prefix}/images/vowkeeper/example.png`
 
-export const glitchRose = "./images/glitch-tool/rose.png"
-export const glitchViking = "./images/glitch-tool/viking.png"
+export const polychatExample = `${prefix}/images/polychat/example.png`
 
-export const projectsExample = "./images/projects/example.png"
+export const glitchRose = `${prefix}/images/glitch-tool/rose.png`
+export const glitchViking = `${prefix}/images/glitch-tool/viking.png`
 
-export const scrabbleBotExample = "./images/scrabblebot/example.png"
+export const projectsExample = `${prefix}/images/projects/example.png`
 
-export const flexmatKiosk = "./images/ilx-system/flexmat-kiosk.png"
-export const flexmatWeb = "./images/ilx-system/flexmat-web.png"
-export const flexmatHardware = "./images/ilx-system/flexmat-hardware.png"
-export const leverandorservice = "./images/ilx-system/leverandorservice.png"
+export const scrabbleBotExample = `${prefix}/images/scrabblebot/example.png`
 
-export const cvVms = "./images/clearvoyage/vms.png"
-export const cvCrew = "./images/clearvoyage/crew.png"
-export const cvSpeedcons = "./images/clearvoyage/speedcons.png"
+export const flexmatKiosk = `${prefix}/images/ilx-system/flexmat-kiosk.png`
+export const flexmatWeb = `${prefix}/images/ilx-system/flexmat-web.png`
+export const flexmatHardware = `${prefix}/images/ilx-system/flexmat-hardware.png`
+export const leverandorservice = `${prefix}/images/ilx-system/leverandorservice.png`
+
+export const cvVms = `${prefix}/images/clearvoyage/vms.png`
+export const cvCrew = `${prefix}/images/clearvoyage/crew.png`
+export const cvSpeedcons = `${prefix}/images/clearvoyage/speedcons.png`
