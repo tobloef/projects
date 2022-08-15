ceb79b9ba7c190ee26064080bf94c02c686b8451
Tobias Løfgren <tobloef@gmail.com>
2022-08-16 01:34:04 +0200

    Fix build

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
 import './globals.css'
+import { BASE_PATH } from "./utils/constants";
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_PATH}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,68 +1,68 @@
-const prefix = "/projects";
+import { BASE_PATH } from "./constants";
 
-export const markantThumbnail = `${prefix}/images/markant/thumbnail.png`;
-export const markantExample = `${prefix}/images/markant/example.png`;
+export const markantThumbnail = `${BASE_PATH}/images/markant/thumbnail.png`;
+export const markantExample = `${BASE_PATH}/images/markant/example.png`;
 
-export const setSolverThumbnail = `${prefix}/images/set-solver/thumbnail.png`;
-export const setSolverExample = `${prefix}/images/set-solver/example.jpeg`;
+export const setSolverThumbnail = `${BASE_PATH}/images/set-solver/thumbnail.png`;
+export const setSolverExample = `${BASE_PATH}/images/set-solver/example.jpeg`;
 
-export const putPixThumbnail = `${prefix}/images/3d-renderer/thumbnail.png`;
+export const putPixThumbnail = `${BASE_PATH}/images/3d-renderer/thumbnail.png`;
 
-export const birdInParadiseThumbnail = `${prefix}/images/bird-in-paradise/thumbnail.jpeg`
+export const birdInParadiseThumbnail = `${BASE_PATH}/images/bird-in-paradise/thumbnail.jpeg`
 
-export const ctfThumbnail = `${prefix}/images/ctf/thumbnail.png`
-export const ctfExample = `${prefix}/images/ctf/example.png`
+export const ctfThumbnail = `${BASE_PATH}/images/ctf/thumbnail.png`
+export const ctfExample = `${BASE_PATH}/images/ctf/example.png`
 
-export const cardFuThumbnail = `${prefix}/images/card-fu/thumbnail.png`
+export const cardFuThumbnail = `${BASE_PATH}/images/card-fu/thumbnail.png`
 
-export const co2Thumbnail = `${prefix}/images/co2/thumbnail.png`
+export const co2Thumbnail = `${BASE_PATH}/images/co2/thumbnail.png`
 
-export const yotoThumbnail = `${prefix}/images/yoto/thumbnail.png`
+export const yotoThumbnail = `${BASE_PATH}/images/yoto/thumbnail.png`
 
-export const roll20DiceScreenshot = `${prefix}/images/roll20-dice/screenshot.png`
+export const roll20DiceScreenshot = `${BASE_PATH}/images/roll20-dice/screenshot.png`
 
-export const text2mindmapScreenshot = `${prefix}/images/text2mindmap/example.png`
+export const text2mindmapScreenshot = `${BASE_PATH}/images/text2mindmap/example.png`
 
-export const faceGeneratorExample = `${prefix}/images/face-generator/example.png`
+export const faceGeneratorExample = `${BASE_PATH}/images/face-generator/example.png`
 
-export const nftFrontpage = `${prefix}/images/nft/frontpage.png`
-export const nftNft = `${prefix}/images/nft/nft.png`
-export const nftNfts = `${prefix}/images/nft/nfts.png`
-export const nftUser = `${prefix}/images/nft/user.png`
-export const nftTrades = `${prefix}/images/nft/trades.png`
+export const nftFrontpage = `${BASE_PATH}/images/nft/frontpage.png`
+export const nftNft = `${BASE_PATH}/images/nft/nft.png`
+export const nftNfts = `${BASE_PATH}/images/nft/nfts.png`
+export const nftUser = `${BASE_PATH}/images/nft/user.png`
+export const nftTrades = `${BASE_PATH}/images/nft/trades.png`
 
-export const creativeExample = `${prefix}/images/creative/example.png`
-export const creativeMidiController = `${prefix}/images/creative/midi-controller.png`
+export const creativeExample = `${BASE_PATH}/images/creative/example.png`
+export const creativeMidiController = `${BASE_PATH}/images/creative/midi-controller.png`
 
-export const neuralnetThumbnail = `${prefix}/images/neuralnet/thumbnail.jpeg`
+export const neuralnetThumbnail = `${BASE_PATH}/images/neuralnet/thumbnail.jpeg`
 
-export const base64Example = `${prefix}/images/base64/example.png`
+export const base64Example = `${BASE_PATH}/images/base64/example.png`
 
-export const mapExample = `${prefix}/images/map/example.png`
+export const mapExample = `${BASE_PATH}/images/map/example.png`
 
-export const nemCvThumbnail = `${prefix}/images/nem-cv/thumbnail.png`
-export const nemCvExample = `${prefix}/images/nem-cv/example.png`
+export const nemCvThumbnail = `${BASE_PATH}/images/nem-cv/thumbnail.png`
+export const nemCvExample = `${BASE_PATH}/images/nem-cv/example.png`
 
-export const fooblogExample = `${prefix}/images/fooblog/example.png`
+export const fooblogExample = `${BASE_PATH}/images/fooblog/example.png`
 
-export const realtimeEditorExample = `${prefix}/images/realtime-editor/example.png`
+export const realtimeEditorExample = `${BASE_PATH}/images/realtime-editor/example.png`
 
-export const vowkeeperExample = `${prefix}/images/vowkeeper/example.png`
+export const vowkeeperExample = `${BASE_PATH}/images/vowkeeper/example.png`
 
-export const polychatExample = `${prefix}/images/polychat/example.png`
+export const polychatExample = `${BASE_PATH}/images/polychat/example.png`
 
-export const glitchRose = `${prefix}/images/glitch-tool/rose.png`
-export const glitchViking = `${prefix}/images/glitch-tool/viking.png`
+export const glitchRose = `${BASE_PATH}/images/glitch-tool/rose.png`
+export const glitchViking = `${BASE_PATH}/images/glitch-tool/viking.png`
 
-export const projectsExample = `${prefix}/images/projects/example.png`
+export const projectsExample = `${BASE_PATH}/images/projects/example.png`
 
-export const scrabbleBotExample = `${prefix}/images/scrabblebot/example.png`
+export const scrabbleBotExample = `${BASE_PATH}/images/scrabblebot/example.png`
 
-export const flexmatKiosk = `${prefix}/images/ilx-system/flexmat-kiosk.png`
-export const flexmatWeb = `${prefix}/images/ilx-system/flexmat-web.png`
-export const flexmatHardware = `${prefix}/images/ilx-system/flexmat-hardware.png`
-export const leverandorservice = `${prefix}/images/ilx-system/leverandorservice.png`
+export const flexmatKiosk = `${BASE_PATH}/images/ilx-system/flexmat-kiosk.png`
+export const flexmatWeb = `${BASE_PATH}/images/ilx-system/flexmat-web.png`
+export const flexmatHardware = `${BASE_PATH}/images/ilx-system/flexmat-hardware.png`
+export const leverandorservice = `${BASE_PATH}/images/ilx-system/leverandorservice.png`
 
-export const cvVms = `${prefix}/images/clearvoyage/vms.png`
-export const cvCrew = `${prefix}/images/clearvoyage/crew.png`
-export const cvSpeedcons = `${prefix}/images/clearvoyage/speedcons.png`
+export const cvVms = `${BASE_PATH}/images/clearvoyage/vms.png`
+export const cvCrew = `${BASE_PATH}/images/clearvoyage/crew.png`
+export const cvSpeedcons = `${BASE_PATH}/images/clearvoyage/speedcons.png`
